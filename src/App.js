3604632aa5016a2f72c6d849143482c7f080421c
dineshdsv3 <dineshdsv3@gmail.com>
2020-07-08 11:27:32 +0530

    Lazy Loading completed

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import Post from './Post';
 import data from './data';
 import Lazyload from 'react-lazyload';
-import Loading from './Loading'
+import Loading from './Loading';
 
 function App() {
-
-  return (
-    <div className="App">
-      <h2>lazy loading demo</h2>
-        <div className="post-container">
-          {data.map((ele) => (
-            <Post id={ele.id} title={ele.title} body={ele.body}/>
-          ))}
-        </div >
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>Lazy Loading Demo</h2>
+			<div className="post-container">
+				{data.map((post) => (
+					<Lazyload height={200} key={post.id} once={true} placeholder={<Loading />}>
+						<Post key={post.id} {...post} />
+					</Lazyload>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Post.js
+++ src/Post.js
@@ -4,6 +4,7 @@ const Post = ({ id, title, body }) => {
 	return (
 		<div className="post">
 			<div className="post-body">
+				<img src={`https://picsum.photos/id/${id}/200/200`} />
 				<h4>{title}</h4>
 				<p>{body}</p>
 			</div>
